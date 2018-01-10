31518cc4ccd148b9549f2635363d2e6e0f74fcef
tarunaz <tmehra@redhat.com>
2018-01-09 19:33:53 -0800

    Update docker-entrypoint.sh

--- 0.X/docker-entrypoint.sh
+++ 0.X/docker-entrypoint.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/dumb-init /bin/sh
 set -e
 
-USER root
-
 # Note above that we run dumb-init as PID 1 in order to reap zombie processes
 # as well as forward signals to all processes in its session. Normally, sh
 # wouldn't do either of these functions so we'd leak zombies as well as do
